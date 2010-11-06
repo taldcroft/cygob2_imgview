f339af6812f5a9c52bbb12afafaf7bed17fb2f99
Tom Aldcroft <taldcroft@gmail.com>
2010-11-06 14:58:54 -0400

    Functioning version of ingest.py using Nick's VOTS tables

--- ingest.py
+++ ingest.py
@@ -1,72 +1,81 @@
 #!/usr/bin/env python
-import sys
-import re
 import os
 
-import pyfits
-import numpy as np
-import asciitable
-import pyyaks.context
-import eventfile
-import files_def
+import vots
 import Ska.DBI
 import cPickle as pickle
 
-src = pyyaks.context.ContextDict('src')
-files = pyyaks.context.ContextDict('files', basedir='/data/cygob2/process/data/baseline/v3')
-files.update(files_def.files)
-
 def id_key(cat, id_):
-    return '{0}-{1:06d}'.format(cat, int(id_))
-
-def get_detections(filename):
-    # lower case, change all whitespace to space, and strip leading/trailing spaces
-    lines = [re.sub(r'\s', ' ', x.lower().strip()) for x in open(filename).readlines()]
-    dets = asciitable.read(lines, Reader=asciitable.CommentedHeader, numpy=False)
-    colnames = dets.dtype.names
-    dets_dict = {}
-    for det in dets:
-        det_dict = dict((name, det[idx]) for idx, name in enumerate(colnames))
-        id_ = det_dict['det_id']
-        det_dict['cat'] = cat
-        det_dict['id'] = id_
-        dets_dict[id_key(cat, id_)] = det_dict
-    return dets_dict
-
-def get_groups(filename):
-    # # Master_number  Group_members  All_matched_flag  Detection_numbers...
-    # 1	9	1	1	539	802	6441	6677	6839	6915	17592	17847	
-    lines = [re.sub(r'\s', ' ', x.lower().strip()) for x in open(filename).readlines()]
-    groups = dict()
-    for line in lines[1:]:
-        vals = line.split()
-        key = int(vals[0])
-        groups[key] = {'det_ids': [id_key(cat, x) for x in vals[3:]]}
-    return groups
-                          
-print 'reading'
-cat = 'cwdet'
-dets = get_detections('all_detections.dat')
-groups = get_groups('new_matches.dat')
+    return '{0}-{1}'.format(cat, id_)
 
 dbfile = 'datastore.db3'
 if os.path.exists(dbfile):
     os.unlink(dbfile)
 db = Ska.DBI.DBI(dbi='sqlite', server=dbfile, autocommit=False)
-try:
-    db.execute('create table dets (key text primary key, val text)')
-    db.execute('create table groups (key integer primary key, val text)')
-except:
-    pass
-
-print 'writing dets'
-for key in sorted(dets):
-    db.insert({'key': key, 'val': pickle.dumps(dets[key])}, 'dets')
-db.commit()
+for table_name, id_type in (('dets', 'text'),
+                            ('groups', 'int')):
+    db.execute("""CREATE TABLE {0} (
+                  id {1} PRIMARY KEY,
+                  ra float,
+                  dec float,
+                  flag int,
+                  info text)""".format(table_name, id_type))
+print 'reading groups'
+hdr, groups = vots.parse_table(open('cat_groups.vots'))
         
+print 'reading assoc_groups_obsids'
+hdr, groups_obsids = vots.parse_table(open('assoc_groups_obsids.vots'))
+groups_obsids_dict = {}
+for group_id, obsid in groups_obsids:
+    group_id = int(group_id)
+    obsid = int(obsid)
+    groups_obsids_dict.setdefault(group_id, []).append(obsid)
+
+print 'reading assoc_groups'
+cats = ('cwdet', 'pwdet', 'vdet', 'mopwdet', 'hdet')
+hdr, groups_detids = vots.parse_table(open('assoc_groups.vots'))
+groups_detids_dict = {}
+for group_id, cat_id, detid, flag, conf in groups_detids:
+    group_id = int(group_id)
+    detid = int(detid)
+    groups_detids_dict.setdefault(group_id, []).append(id_key(cats[cat_id], detid))
+
+for cat in ('cwdet', 'pwdet'):
+    print 'reading {0} dets'.format(cat)
+    hdr, dets = vots.parse_table(open('cat_{0}.vots'.format(cat)))
+
+    print 'writing {0} dets'.format(cat)
+    for det in dets:
+        info = dict((x, det[x].tolist()) for x in det.dtype.names)
+        db.insert({'id': id_key(cat, det['id']),
+                   'ra': det['ra'],
+                   'dec': det['dec'],
+                   'flag': 0,
+                   'info': pickle.dumps(info)}, 'dets')
+    db.commit()
+
 print 'writing groups'
-for key in sorted(groups):
-    db.insert({'key': key, 'val': pickle.dumps(groups[key])}, 'groups')
+for i, group in enumerate(groups):
+    try:
+        info = dict((x, group[x].tolist()) for x in group.dtype.names)
+        info['obsids'] = groups_obsids_dict[group['id']]
+        info['detids'] = groups_detids_dict[group['id']]
+        info['views'] = []
+        info['comment'] = ""
+        db.insert({'id': group['id'],
+                   'ra': group['ra'],
+                   'dec': group['dec'],
+                   'flag': 0,
+                   'info': pickle.dumps(info)}, 'groups')
+    except Exception, err:
+        print 'Failure for group_id={0}: {1}'.format(group['id'], err)
+        pass
 db.commit()
 
+for table_name, id_type in (('dets', 'text'),
+                            ('groups', 'int')):
+    for col_name in ('ra', 'dec'):
+        db.execute("CREATE INDEX idx_{0}_{1} ON {0} ( {1} )".format(
+            table_name, col_name))
 
+db.commit()
